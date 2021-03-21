f897d59580be00ba75fe5fc8655eb7080a55326e
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-03-22 00:11:33 +0900

    chore : spring 프로젝트 생성

---
+++ src/main/java/com/teleworkfreak/ondolbangv2/Ondolbangv2Application.java
@@ -0,0 +1,13 @@
+package com.teleworkfreak.ondolbangv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ondolbangv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ondolbangv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/teleworkfreak/ondolbangv2/Ondolbangv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleworkfreak.ondolbangv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ondolbangv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

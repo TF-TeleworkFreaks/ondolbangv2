8441963a7fe798daa120abecaf2f4c20ab9fbf1c
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-05-01 02:39:10 +0900

    Add ItemsConfig.java
    관심사를 분리하여 DIP에 위배되지 않게 함

---
+++ src/main/java/com/teleworkfreak/ondolbangv2/items/ItemsConfig.java
@@ -0,0 +1,20 @@
+package com.teleworkfreak.ondolbangv2.items;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class ItemsConfig {
+    private final EntityManager em;
+
+    public ItemsConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public ItemsRepository itemsRepository() {
+        return new JPAItemsRepository(em);
+    }
+}
